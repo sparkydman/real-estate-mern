84655ca95f7e31808a53afd2f53213a09ef6230f
Chigozie Ugwuede <sparkyglobe@gmail.com>
2021-02-08 02:05:06 +0100

    Updated

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.use('/api/v1/dm', dm);
 
 app.use(errorHandler);
 
-// server static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
