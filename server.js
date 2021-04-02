1ee661e778cb598cd6421acc0c594123d58a5915
Ugwuede Chigozie <sparkyglobe@gmail.com>
2021-04-02 16:59:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -31,9 +31,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'production'
-        ? 'https://real-estate-space.herokuapp.com'
-        : 'http://localhost:3000',
+    origin: 'http://localhost:3000',
 );
 
 // add express json middleware
