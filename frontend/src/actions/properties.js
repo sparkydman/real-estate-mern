bffdbce6cc9aa48763dd2e470155aefdeba07fb8
Ugwuede Chigozie <sparkyglobe@gmail.com>
2021-04-02 17:09:48 +0100

    Update properties.js

--- frontend/src/actions/properties.js
+++ frontend/src/actions/properties.js
@@ -9,7 +9,7 @@ export const getAllProperties = () => async (dispatch) => {
   try {
     dispatch({ type: GET_ALL_PROPERTIES_REQUEST });
 
-    const { data } = await axios.get('/api/v1/property');
+    const { data } = await axios.get('/property');
     dispatch({
       type: GET_ALL_PROPERTIES_SUCCESS,
       payload: data,
